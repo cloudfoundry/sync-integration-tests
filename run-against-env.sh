e7f6715ebf78777db4079ed7ee63b30ca2b0b795
Aaron Hurley <ahurley@pivotal.io>
2018-04-16 14:53:29 -0700

    Add an option to run routing tests
    [#156765870]
    
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

--- run-against-env.sh
+++ run-against-env.sh
@@ -8,6 +8,7 @@ set -exu
 : "${BOSH_API_INSTANCE:="api/0"}"
 : "${CF_SKIP_SSL_VALIDATION:="true"}"
 : "${DEPLOYMENT_VARS_FILENAME:="deployment-vars.yml"}"
+: "${RUN_ROUTING_TESTS:="false"}"
 :  BBL_STATE_DIR
 :  VARS_STORE_PATH
 :  CF_APPS_DOMAIN
@@ -19,15 +20,25 @@ echo "$config_dir"
 
 pushd "${VARS_STORE_PATH}" > /dev/null
 set +x
+  CF_ADMIN_PASSWORD="$(bosh int --path /cf_admin_password ${DEPLOYMENT_VARS_FILENAME})"
+
   bosh_certs_dir=$(mktemp -d /tmp/sits-bosh-certs.XXXXXX)
 
   mkdir -p "${bosh_certs_dir}/diego-certs/bbs-certs"
   bbs_cert_path="${bosh_certs_dir}/diego-certs/bbs-certs/client.crt"
   bbs_key_path="${bosh_certs_dir}/diego-certs/bbs-certs/client.key"
 
-  CF_ADMIN_PASSWORD="$(bosh int --path /cf_admin_password ${DEPLOYMENT_VARS_FILENAME})"
   bosh int --path /diego_bbs_client/certificate "${DEPLOYMENT_VARS_FILENAME}" > "${bbs_cert_path}"
   bosh int --path /diego_bbs_client/private_key "${DEPLOYMENT_VARS_FILENAME}" > "${bbs_key_path}"
+
+  if [ "${RUN_ROUTING_TESTS}" = true ]; then
+    mkdir -p "${bosh_certs_dir}/routing-certs/copilot-certs"
+    copilot_client_cert_path="${bosh_certs_dir}/routing-certs/copilot-certs/client.crt"
+    copilot_client_key_path="${bosh_certs_dir}/routing-certs/copilot-certs/client.key"
+    bosh int --path /copilot_client/certificate "${DEPLOYMENT_VARS_FILENAME}" > "${copilot_client_cert_path}"
+    bosh int --path /copilot_client/private_key "${DEPLOYMENT_VARS_FILENAME}" > "${copilot_client_key_path}"
+  fi
+
 set -x
 popd > /dev/null
 
@@ -49,6 +60,8 @@ set +x
   "cf_apps_domain": "${CF_APPS_DOMAIN}",
   "bbs_client_cert": "${bbs_cert_path}",
   "bbs_client_key": "${bbs_key_path}",
+  "copilot_client_cert": "${copilot_client_cert_path}",
+  "copilot_client_key": "${copilot_client_key_path}",
   "bosh_binary": "${BOSH_BINARY}",
   "bosh_api_instance": "${BOSH_API_INSTANCE}",
   "bosh_deployment_name": "${BOSH_DEPLOYMENT_NAME}",
