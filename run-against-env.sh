c21c812bb5182006cbeb84e23d32c3f4b218fb65
Utako Ueda <uueda@pivotal.io>
2018-06-01 11:25:48 -0700

    run-against-env.sh can use credhub

--- run-against-env.sh
+++ run-against-env.sh
@@ -3,57 +3,70 @@
 set -exu
 
 # ENV
-: "${BOSH_BINARY:="bosh"}"
-: "${BOSH_DEPLOYMENT_NAME:="cf"}"
-: "${BOSH_API_INSTANCE:="api/0"}"
-: "${CF_SKIP_SSL_VALIDATION:="true"}"
-: "${DEPLOYMENT_VARS_FILENAME:="deployment-vars.yml"}"
-: "${RUN_ROUTING_TESTS:="false"}"
-:  BBL_STATE_DIR
-:  VARS_STORE_PATH
-:  CF_APPS_DOMAIN
+main() {
+  : "${BOSH_BINARY:="bosh"}"
+  : "${BOSH_DEPLOYMENT_NAME:="cf"}"
+  : "${BOSH_API_INSTANCE:="api/0"}"
+  : "${CF_SKIP_SSL_VALIDATION:="true"}"
+  : "${DEPLOYMENT_VARS_FILENAME:="deployment-vars.yml"}"
+  : "${RUN_ROUTING_TESTS:="false"}"
+  : "${VARS_STORE_PATH:=""}"
+  :  BBL_STATE_DIR
+  :  CF_APPS_DOMAIN
 
-# INPUTS
-config_dir=$(mktemp -d /tmp/sits-config.XXXXXX)
-export CONFIG=${config_dir}/config.json
-echo "$config_dir"
+  local copilot_client_cert_path
+  local copilot_client_key_path
 
-pushd "${VARS_STORE_PATH}" > /dev/null
-set +x
-  CF_ADMIN_PASSWORD="$(bosh int --path /cf_admin_password ${DEPLOYMENT_VARS_FILENAME})"
+  # INPUTS
+  config_dir=$(mktemp -d /tmp/sits-config.XXXXXX)
+  export CONFIG=${config_dir}/config.json
+  echo "$config_dir"
 
   bosh_certs_dir=$(mktemp -d /tmp/sits-bosh-certs.XXXXXX)
-
   mkdir -p "${bosh_certs_dir}/diego-certs/bbs-certs"
   bbs_cert_path="${bosh_certs_dir}/diego-certs/bbs-certs/client.crt"
   bbs_key_path="${bosh_certs_dir}/diego-certs/bbs-certs/client.key"
 
-  bosh int --path /diego_bbs_client/certificate "${DEPLOYMENT_VARS_FILENAME}" > "${bbs_cert_path}"
-  bosh int --path /diego_bbs_client/private_key "${DEPLOYMENT_VARS_FILENAME}" > "${bbs_key_path}"
-
-  copilot_client_cert_path=""
-  copilot_client_key_path=""
   if [ "${RUN_ROUTING_TESTS}" = true ]; then
     mkdir -p "${bosh_certs_dir}/routing-certs/copilot-certs"
     copilot_client_cert_path="${bosh_certs_dir}/routing-certs/copilot-certs/client.crt"
     copilot_client_key_path="${bosh_certs_dir}/routing-certs/copilot-certs/client.key"
-    bosh int --path /copilot_client/certificate "${DEPLOYMENT_VARS_FILENAME}" > "${copilot_client_cert_path}"
-    bosh int --path /copilot_client/private_key "${DEPLOYMENT_VARS_FILENAME}" > "${copilot_client_key_path}"
   fi
 
-set -x
-popd > /dev/null
+  set +x
+    if [[ ! -z ${VARS_STORE_PATH} ]]; then
+      pushd "${VARS_STORE_PATH}" > /dev/null
+        CF_ADMIN_PASSWORD="$(bosh int --path /cf_admin_password ${DEPLOYMENT_VARS_FILENAME})"
+        bosh int --path /diego_bbs_client/certificate "${DEPLOYMENT_VARS_FILENAME}" > "${bbs_cert_path}"
+        bosh int --path /diego_bbs_client/private_key "${DEPLOYMENT_VARS_FILENAME}" > "${bbs_key_path}"
 
-pushd "${BBL_STATE_DIR}" > /dev/null
-set +x
-  keys_dir=$(mktemp -d /tmp/sits-keys-dir.XXXXXX)
-  bosh_ca_cert="${keys_dir}/bosh-ca.crt"
-  bbl director-ca-cert > "${bosh_ca_cert}"
-  bosh_gw_private_key="${keys_dir}/bosh.pem"
-  bbl ssh-key > "${bosh_gw_private_key}"
-  chmod 600 "${bosh_gw_private_key}"
+        if [ "${RUN_ROUTING_TESTS}" = true ]; then
+          bosh int --path /copilot_client/certificate "${DEPLOYMENT_VARS_FILENAME}" > "${copilot_client_cert_path}"
+          bosh int --path /copilot_client/private_key "${DEPLOYMENT_VARS_FILENAME}" > "${copilot_client_key_path}"
+        fi
+      popd > /dev/null
+    else
+      CF_ADMIN_PASSWORD="$(fetch_credhub_cred cf_admin_password "")"
+      echo "$(fetch_credhub_cred diego_bbs_client ".certificate")" > "${bbs_cert_path}"
+      echo "$(fetch_credhub_cred diego_bbs_client ".private_key")" > "${bbs_key_path}"
 
-  cat > "$CONFIG" <<EOF
+      if [ "${RUN_ROUTING_TESTS}" = true ]; then
+        echo "$(fetch_credhub_cred copilot_client ".certificate")" > "${copilot_client_cert_path}"
+        echo "$(fetch_credhub_cred copilot_client ".private_key")" > "${copilot_client_key_path}"
+      fi
+    fi
+  set -x
+
+  pushd "${BBL_STATE_DIR}" > /dev/null
+    set +x
+      keys_dir=$(mktemp -d /tmp/sits-keys-dir.XXXXXX)
+      bosh_ca_cert="${keys_dir}/bosh-ca.crt"
+      bbl director-ca-cert > "${bosh_ca_cert}"
+      bosh_gw_private_key="${keys_dir}/bosh.pem"
+      bbl ssh-key > "${bosh_gw_private_key}"
+      chmod 600 "${bosh_gw_private_key}"
+
+      cat > "$CONFIG" <<EOF
 {
   "cf_api": "api.${CF_APPS_DOMAIN}",
   "cf_admin_user": "admin",
@@ -76,16 +89,26 @@ set +x
   "bosh_gw_private_key": "${bosh_gw_private_key}"
 }
 EOF
-set -x
-popd > /dev/null
+    set -x
+  popd > /dev/null
 
-cd $(dirname "${BASH_SOURCE[0]}")/..
+  cd $(dirname "${BASH_SOURCE[0]}")/..
 
-go install code.cloudfoundry.org/sync-integration-tests/vendor/github.com/onsi/ginkgo/ginkgo
-ginkgo -r -nodes=3 -randomizeAllSpecs
+  go install code.cloudfoundry.org/sync-integration-tests/vendor/github.com/onsi/ginkgo/ginkgo
+  ginkgo -r -nodes=3 -randomizeAllSpecs
 
-rm -r "${config_dir}"
-rm -r "${bosh_certs_dir}"
-rm -r "${keys_dir}"
+  rm -r "${config_dir}"
+  rm -r "${bosh_certs_dir}"
+  rm -r "${keys_dir}"
 
-exit 0
+  exit 0
+}
+
+fetch_credhub_cred() {
+  var=$1
+  field=$2
+
+  credhub find -j -n "${var}" | jq -r .credentials[].name | xargs credhub get -j -n | jq -r ".value${field}"
+}
+
+main
