f46ac5961de32727e0b02fcd3723f836041a365d
Connor Braa <cbraa@pivotal.io>
2019-01-11 16:47:20 -0800

    update run-bosh-lite to accomodate new boshlite regime
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- run-bosh-lite.sh
+++ run-bosh-lite.sh
@@ -2,64 +2,52 @@
 
 set -eu
 
-# ENV
-: "${CF_VARS_STORE:=$HOME/deployments/vbox/deployment-vars.yml}"
-: "${BOSH_VARS_STORE:=$HOME/deployments/vbox/creds.yml}"
+fetch_credhub_cred() {
+  var=$1
+  field=$2
 
-if [ ! -f "${CF_VARS_STORE}" ]; then
-  echo "Unable to find CF vars store at ${CF_VARS_STORE}. Specify your vars store with \$CF_VARS_STORE!"
-  exit 1
-fi
-if [ ! -f "${BOSH_VARS_STORE}" ]; then
-  echo "Unable to find BOSH vars store at ${BOSH_VARS_STORE}. Specify your vars store with \$BOSH_VARS_STORE!"
-  exit 1
-fi
+  credhub find -j -n "${var}" | jq -r .credentials[].name | xargs credhub get -j -n | jq -r ".value${field}"
+}
 
 if [ $(uname) == 'Darwin' ]; then
   config_path=$(mktemp -d -t 'sits')
 else
   config_path=$(mktemp -d)
 fi
-trap "{ rm -rf ${config_path}; }" EXIT
 
 bbs_client_cert_path="${config_path}/bbs.crt"
-bosh interpolate --path /cc_tls/certificate "${CF_VARS_STORE}" > "${bbs_client_cert_path}"
 bbs_client_key_path="${config_path}/bbs.key"
-bosh interpolate --path /cc_tls/private_key "${CF_VARS_STORE}" > "${bbs_client_key_path}"
+echo "$(fetch_credhub_cred diego_bbs_client ".certificate")" > "${bbs_client_cert_path}"
+echo "$(fetch_credhub_cred diego_bbs_client ".private_key")" > "${bbs_client_key_path}"
 
 director_ca_cert="${config_path}/director.crt"
-bosh interpolate --path /director_ssl/ca "${BOSH_VARS_STORE}" > "${director_ca_cert}"
+echo "${BOSH_CA_CERT}" > "${director_ca_cert}"
+
 director_gw_key="${config_path}/director_gw.key"
-bosh interpolate --path /jumpbox_ssh/private_key "${BOSH_VARS_STORE}" > "${director_gw_key}"
+echo "${BOSH_GW_PRIVATE_KEY_CONTENTS}" > "${director_gw_key}"
+
 chmod 600 "${director_gw_key}"
-
-set +e
-bosh_password="$(bosh interpolate --path /admin_password "${BOSH_VARS_STORE}" 2> /dev/null)"
-exit_code="$?"
-set -e
-
-if [ "${exit_code}" != 0 ]; then
-  bosh_password="admin"
-fi
 
 export CONFIG=${config_path}/config.json
 cat > "$CONFIG" <<EOF
 {
-  "cf_api": "api.bosh-lite.com",
+  "cf_api": "api.${BOSH_LITE_DOMAIN}",
   "cf_admin_user": "admin",
-  "cf_admin_password": "admin",
+  "cf_admin_password": "$(fetch_credhub_cred cf_admin_password "")",
   "cf_skip_ssl_validation": true,
-  "cf_apps_domain": "bosh-lite.com",
+  "cf_apps_domain": "${BOSH_LITE_DOMAIN}",
   "bbs_client_cert": "${bbs_client_cert_path}",
   "bbs_client_key": "${bbs_client_key_path}",
   "bosh_ca_cert": "${director_ca_cert}",
   "bosh_client": "admin",
-  "bosh_client_secret": "${bosh_password}",
-  "bosh_environment": "https://192.168.50.6:25555",
-  "bosh_gw_user": "jumpbox",
-  "bosh_gw_host": "192.168.50.6",
+  "bosh_client_secret": "${BOSH_CLIENT_SECRET}",
+  "bosh_environment": "${BOSH_ENVIRONMENT}",
+  "bosh_gw_user": "${BOSH_GW_USER}",
+  "bosh_gw_host": "${BOSH_GW_HOST}",
   "bosh_gw_private_key": "${director_gw_key}"
 }
 EOF
+
+cat ${CONFIG}
 
 ginkgo -nodes=3
